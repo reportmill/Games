9769a884112ba9b2d81e0d3bf7aa32c2e4635583
Jeff Martin <jeff@reportmill.com>
2019-07-16 15:47:34 -0500

    PlayView: Added code to replace range of selected gems

--- src/jewels/PlayView.java
+++ src/jewels/PlayView.java
@@ -14,71 +14,38 @@ public class PlayView extends ParentView {
     // The last gem hit by mouse press
     Gem         _pressGem;
     
-    // The timer
-    ViewTimer   _timer = new ViewTimer(25, t -> timerFired());
-    
     // The grid width/height
     public static int GRID_WIDTH = 8;
     public static int GRID_HEIGHT = 8;
     public static int TILE_SIZE = 64;
     public static int BORDER_SIZE = 2;
     public static int GEM_SPEED = 140;
-    public static int ROW_SPEEDS[] = new int[GRID_WIDTH];
 
 /**
  * Creates a PlayView.
  */
 public PlayView()
 {
-    // Create background texture
+    // Set background texture
     Image img = Image.get(PlayView.class, "pkg.images/Cloth.jpg");
-    img.addLoadListener(pc -> backImageLoaded(img));
+    img.addLoadListener(pc -> setFill(new ImagePaint(img, BORDER_SIZE, BORDER_SIZE, 64, 64, false)));
     
     // Set PlayView fill, border, PrefSize
     setBorder(Color.BLACK, 2);
     setPrefSize(GRID_WIDTH*TILE_SIZE + BORDER_SIZE*2, GRID_HEIGHT*TILE_SIZE + BORDER_SIZE*2);
     setClipToBounds(true);
-    enableEvents(MousePress, MouseDrag);
+    enableEvents(MousePress, MouseDrag, MouseRelease);
 }
 
 /**
  * Starts the game.
  */
-public void startGame()
-{
-    // Set (or reset) row speeds
-    for(int i=0;i<GRID_WIDTH;i++) ROW_SPEEDS[i] = 120 + (int)Math.round(Math.random()*40);
-    
-    // Clear/reload gems
-    clearGems();
-    reloadGems();
-}
-
-/**
- * Called when background texture image is loaded.
- */
-void backImageLoaded(Image anImage)
-{
-    anImage.getPainter();
-    Painter pntr = anImage.getPainter(); pntr.setColor(new Color("#44998833"));
-    pntr.drawRect(.5,.5, anImage.getWidth()-1, anImage.getHeight()-1);
-    ImagePaint paint = new ImagePaint(anImage, BORDER_SIZE, BORDER_SIZE, 64, 64, false);
-    setFill(paint);
-}
+public void startGame()  { reloadGems(); }
 
 /**
  * Pauses game.
  */
-public void pauseGame()
-{
-    if(_timer.isRunning()) _timer.pause();
-    else _timer.start();
-}
-
-/**
- * Called when timer fires.
- */
-void timerFired()  { }
+public void pauseGame()  { }
 
 /**
  * Returns the gem at given x/y.
@@ -98,8 +65,7 @@ Gem setGem(Gem aGem, int aCol, int aRow)
     
     // Handle null
     if(aGem==null) {
-        return oldGem;
-    }
+        return oldGem; }
     
     // Handle setting new gem
     aGem.setColRow(aCol, aRow);
@@ -112,18 +78,16 @@ Gem setGem(Gem aGem, int aCol, int aRow)
 /**
  * Sets the gem at given x/y.
  */
-void setGemAnimated(Gem aGem, int aCol, int aRow)
+void setGemAnimated(Gem aGem, int aCol, int aRow, int aDelay)
 {
     // Sets gem
     double x0 = aGem!=null? aGem.getX() + aGem.getTransX() : 0;
     double y0 = aGem!=null? aGem.getY() + aGem.getTransY() : 0;
-    Gem oldGem = setGem(aGem, aCol, aRow);
+    Gem oldGem = setGem(aGem, aCol, aRow); if(aGem==oldGem) return;
     
     // If replacing with null, animate out and remove
     if(aGem==null) {
-        oldGem.getAnimCleared(500).setScale(.01).setOpacity(.1).setOnFinish(a -> removeChild(oldGem)).play();
-        return;
-    }
+        tetris.Explode.explode(oldGem, 0); return; }
     
     // Animate new gem into place
     double x1 = aGem.getX() + aGem.getTransX();
@@ -131,7 +95,7 @@ void setGemAnimated(Gem aGem, int aCol, int aRow)
     double dx = x1 - x0, dy = y1 - y0, dist = Math.max(Math.abs(dx), Math.abs(dy));
     aGem.setTransX(-dx); aGem.setTransY(-dy);
     int time = (int)Math.round(dist*GEM_SPEED/TILE_SIZE);
-    aGem.getAnimCleared(time).setTransX(0).setTransY(0).setLinear().play();
+    aGem.getAnimCleared(aDelay).getAnim(aDelay+time).setTransX(0).setTransY(0).setLinear().play();
 }
 
 /**
@@ -141,56 +105,6 @@ Gem getGemAtXY(double aX, double aY)
 {
     GridXY gpnt = localToGrid(aX, aY);
     return getGem(gpnt.x, gpnt.y);
-}
-
-/**
- * Returns the jewel for slot row/col, creating a new one if needed.
- */
-Gem getGemAboveColRow(int aX, int aY)
-{
-    for(int i=aY-1;i>=0;i--) { Gem gem = getGem(aX, i);
-        if(gem!=null) {
-            setGem(null, aX, i); return gem; } }
-    
-    Gem gem = new Gem();
-    Point pnt = gridToLocal(aX, -1);
-    for(int i=0;i<GRID_HEIGHT;i++) { Gem g = _gems[aX][i]; if(g==null) continue;
-        pnt.y = Math.min(pnt.y, g.getY() + g.getTransY() - TILE_SIZE); }
-    gem.setXY(pnt.x, pnt.y);
-    addChild(gem);
-    return gem;
-}
-
-/**
- * Reloads the field of gems.
- */
-void reloadGems()
-{
-    for(int j=GRID_HEIGHT-1;j>=0;j--) {
-        for(int i=0;i<GRID_WIDTH;i++) {
-            if(_gems[i][j]==null) {
-                Gem gem = getGemAboveColRow(i, j);
-                double oldY = gem.getY() - gem.getTransY();
-                Point pnt = gridToLocal(i, j); gem.setY(pnt.y);
-                double dy = gem.getY() - oldY;
-                gem.setTransY(-dy);
-                int time = (int)Math.round(dy*ROW_SPEEDS[i]/TILE_SIZE);
-                gem.getAnimCleared(time).setTransY(0).setLinear().play();
-                _gems[i][j] = gem; gem.setColRow(i,j); //setGem(gem, i, j);
-            }
-        }
-    }
-}
-
-/**
- * Clears the gems.
- */
-void clearGems()
-{
-    for(int i=0;i<GRID_WIDTH;i++) for(int j=0;j<GRID_HEIGHT;j++) { Gem gem = _gems[i][j];
-        if(gem!=null) removeChild(gem);
-        setGem(null, i, j);
-    }
 }
 
 /**
@@ -209,71 +123,78 @@ public GridXY localToGrid(double aX, double aY)
 }
 
 /**
- * Swaps two gems.
+ * Reloads the gems.
  */
-public void swapGems(Gem aGem1, Gem aGem2)
+void reloadGems()
 {
-    // Swap the two gems
-    int col1 = aGem1.getCol(), row1 = aGem1.getRow();
-    int col2 = aGem2.getCol(), row2 = aGem2.getRow();
-    setGemAnimated(aGem1, col2, row2);
-    setGemAnimated(aGem2, col1, row1);
-    
-    // Check and clear matched gems
-    checkAndClearAllMatches();
+    removeChildren();
+    for(int i=0;i<GRID_WIDTH;i++) for(int j=0;j<GRID_HEIGHT;j++) _gems[i][j] = null;
+    clearGems(0, 0, GRID_WIDTH-1, GRID_HEIGHT-1);
 }
 
 /**
- * Checks for matches on all gems and clears them.
+ * Clears the gems in rect from given col/row to second col/row.
  */
-void checkAndClearAllMatches()
+void clearGems(int aCol0, int aRow0, int aCol1, int aRow1)
 {
-    for(int i=0;i<GRID_WIDTH;i++)
-        for(int j=0;j<GRID_HEIGHT;j++)
-            checkAndClearGemMatches(i, j);
-}
-
-/**
- * Checks for matches on given gem and clears them.
- */
-void checkAndClearGemMatches(int aCol, int aRow)
-{
-    Match m = getMatch(aCol, aRow);
-    if(m!=null) {
-        GridXY pnts[] = m.getPoints();
-        for(GridXY pnt : pnts)
-            setGemAnimated(null, pnt.x, pnt.y);
+    for(int i=aCol0;i<=aCol1;i++) {
+        for(int j=aRow0;j<=aRow1;j++) {
+            setGemAnimated(null, i, j, 0); }
+        int delCount = aRow1 - aRow0 + 1;
+        int len = aRow1 + 1;
+        copyGems(i, -delCount, 0, len);
     }
 }
 
 /**
- * Returns any match found at given grid xy.
+ * Clears the gems in rect from given col/row to second col/row.
  */
-public Match getMatch(int aCol, int aRow)
+void copyGems(int aCol, int srcRow, int dstRow, int aLen)
 {
-    Gem gem = getGem(aCol, aRow); if(gem==null) return null;
-    int gid = gem.getId();
-    int col0 = aCol, row0 = aRow, col1 = aCol, row1 = aRow;
+    // Get source gems (create/position/add if above bounds)
+    Gem gems[] = new Gem[aLen];
+    for(int i=0;i<aLen;i++) {
+        Gem gem = getGem(aCol, srcRow+i);
+        if(gem==null) {
+            gem = new Gem();
+            Point pnt = gridToLocal(aCol, srcRow+i);
+            gem.setXY(pnt.x, pnt.y);
+            addChild(gem);
+        }
+        gems[i] = gem;
+    }
     
-    for(int i=aCol-1;i>=0;i--) if(getGemId(i, aRow)==gid) col0--; else break;
-    for(int i=aCol+1;i<GRID_WIDTH;i++) if(getGemId(i, aRow)==gid) col1++; else break;
-    for(int i=aRow-1;i>=0;i--) if(getGemId(aCol, i)==gid) row0--; else break;
-    for(int i=aRow+1;i<GRID_HEIGHT;i++) if(getGemId(aCol, i)==gid) row1++; else break;
-    int dx = col1 - col0; if(dx<2) col0 = col1 = aCol;
-    int dy = row1 - row0; if(dy<2) row0 = row1 = aRow;
-    if(dx<2 && dy<2) return null;
-    return new Match(aCol, aRow, col0, row0, col1, row1);
+    // Set gems
+    for(int i=0;i<aLen;i++) { Gem gem = gems[i];
+        setGemAnimated(gem, aCol, dstRow + i, 0);
+    }
 }
-
-/**
- * Returns whether gem at col/row matches id.
- */
-public int getGemId(int aCol, int aRow)  { Gem g = getGem(aCol, aRow); return g!=null? g.getId() : -1; }
 
 /**
  * Handle events.
  */
 protected void processEvent(ViewEvent anEvent)
+{
+    // Handle MouseDown
+    if(anEvent.isMousePress()) {
+        _pressGem = getGemAtXY(anEvent.getX(), anEvent.getY());
+    }
+
+    // Handle MouseRelease
+    else if(anEvent.isMouseRelease() && _pressGem!=null) {
+        ViewEvent mouseDown = ViewUtils.getMouseDown().copyForView(this);
+        GridXY pnt0 = localToGrid(mouseDown.getX(), mouseDown.getY());
+        GridXY pnt1 = localToGrid(anEvent.getX(), anEvent.getY());
+        int col0 = Math.min(pnt0.x, pnt1.x), row0 = Math.min(pnt0.y, pnt1.y);
+        int col1 = Math.max(pnt0.x, pnt1.x), row1 = Math.max(pnt0.y, pnt1.y);
+        clearGems(col0, row0, col1, row1);
+    }
+}
+
+/**
+ * Handle events.
+ */
+protected void processEventGame(ViewEvent anEvent)
 {
     // Handle MouseDown
     if(anEvent.isMousePress()) {
@@ -295,9 +216,81 @@ protected void processEvent(ViewEvent anEvent)
 }
 
 /**
+ * Swaps two gems.
+ */
+public void swapGems(Gem aGem1, Gem aGem2)
+{
+    // Swap the two gems
+    int col1 = aGem1.getCol(), row1 = aGem1.getRow();
+    int col2 = aGem2.getCol(), row2 = aGem2.getRow();
+    setGemAnimated(aGem1, col2, row2, 0);
+    setGemAnimated(aGem2, col1, row1, 0);
+    
+    // Check and clear matched gems
+    checkAndClearAllMatches();
+}
+
+/**
+ * Checks for matches on all gems and clears them.
+ */
+void checkAndClearAllMatches()
+{
+    for(int i=0;i<GRID_WIDTH;i++)
+        for(int j=0;j<GRID_HEIGHT;j++)
+            checkAndClearGemMatches(i, j);
+}
+
+/**
+ * Checks for matches on given gem and clears them.
+ */
+void checkAndClearGemMatches(int aCol, int aRow)
+{
+    Match m = getMatch(aCol, aRow);
+    if(m!=null)
+        clearMatch(m);
+}
+
+/**
+ * Returns any match found at given grid xy.
+ */
+public Match getMatch(int aCol, int aRow)
+{
+    // Get gem at col/row and gem info (just return if null)
+    Gem gem = getGem(aCol, aRow); if(gem==null) return null;
+    int gid = gem.getId(), col0 = aCol, row0 = aRow, col1 = aCol, row1 = aRow;
+    
+    // Iterate left/right and up/down to find matching gem extents
+    for(int i=aCol-1;i>=0;i--) if(getGemId(i, aRow)==gid) col0--; else break;
+    for(int i=aCol+1;i<GRID_WIDTH;i++) if(getGemId(i, aRow)==gid) col1++; else break;
+    for(int i=aRow-1;i>=0;i--) if(getGemId(aCol, i)==gid) row0--; else break;
+    for(int i=aRow+1;i<GRID_HEIGHT;i++) if(getGemId(aCol, i)==gid) row1++; else break;
+    
+    // If extends exceed 2 in either direction, return match
+    int dx = col1 - col0; if(dx<2) col0 = col1 = aCol;
+    int dy = row1 - row0; if(dy<2) row0 = row1 = aRow;
+    if(dx<2 && dy<2) return null;
+    return new Match(aCol, aRow, col0, row0, col1, row1);
+}
+
+/**
+ * Clears a given match.
+ */
+void clearMatch(Match aMatch)
+{
+    GridXY pnts[] = aMatch.getPoints();
+    for(GridXY pnt : pnts)
+        setGemAnimated(null, pnt.x, pnt.y, 0);
+}
+
+/**
+ * Returns whether gem at col/row matches id.
+ */
+int getGemId(int aCol, int aRow)  { Gem g = getGem(aCol, aRow); return g!=null? g.getId() : -1; }
+
+/**
  * Returns the drag change.
  */
-public Size getDragChange(ViewEvent anEvent)
+Size getDragChange(ViewEvent anEvent)
 {
     Point pnt1 = anEvent.getPoint();
     Point pnt0 = ViewUtils.getMouseDown().getPoint(anEvent.getView());
@@ -311,6 +304,7 @@ public Size getDragChange(ViewEvent anEvent)
 static class GridXY {
     public int x, y;
     public GridXY(int aX, int aY)  { x = aX; y = aY; }
+    public String toString() { return "GridXY: " + x + ',' + y; }
 }
 
 /**
